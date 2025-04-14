f4a6b522d2c986a5d36a7d871e51a1d73662585f
Rafael <rafa92muhitdinov@gmail.com>
2025-04-14 21:23:33 +0500

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
 
   return (
     <div className="flex justify-center gap-4 h-screen">
-      <iframe src="https://learning-words-flame.vercel.app" className="w-[500px] h-full border" />
-      <iframe src="https://learning-words-flame.vercel.app" className="w-[500px] h-full border" />
+      <iframe src="rafa992.github.io/react-cinephile/" className="w-[500px] h-full border" />
+      <iframe src="rafa992.github.io/react-cinephile/" className="w-[500px] h-full border" />
     </div>
   );
 }
